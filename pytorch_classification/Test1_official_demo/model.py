f9c056544058bb61933f99ee068c6416c9d888d7
huanghaihang <huanghaihangm@gmail.com>
2021-08-12 21:28:37 +0800

    8.12 LeNet

--- pytorch_classification/Test1_official_demo/model.py
+++ pytorch_classification/Test1_official_demo/model.py
@@ -12,16 +12,15 @@ class LeNet(nn.Module):
         self.fc1 = nn.Linear(32*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-
+#定义正向传播过程
     def forward(self, x):
         x = F.relu(self.conv1(x))    # input(3, 32, 32) output(16, 28, 28)
         x = self.pool1(x)            # output(16, 14, 14)
         x = F.relu(self.conv2(x))    # output(32, 10, 10)
         x = self.pool2(x)            # output(32, 5, 5)
-        x = x.view(-1, 32*5*5)       # output(32*5*5)
+        x = x.view(-1, 32*5*5)       # output(32*5*5)，通过view把数据展平成一维向量(batch,节点个数)
         x = F.relu(self.fc1(x))      # output(120)
         x = F.relu(self.fc2(x))      # output(84)
         x = self.fc3(x)              # output(10)
         return x
-
 
--- pytorch_classification/Test1_official_demo/train.py
+++ pytorch_classification/Test1_official_demo/train.py
@@ -4,18 +4,20 @@ import torch.nn as nn
 from model import LeNet
 import torch.optim as optim
 import torchvision.transforms as transforms
-
-
+import matplotlib.pyplot as plt
+import numpy as np
+#
+#
 def main():
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    # 50000张训练图片
-    # 第一次使用时要将download设置为True才会自动去下载数据集
+#     # 50000张训练图片
+#     # 第一次使用时要将download设置为True才会自动去下载数据集
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
                                              download=False, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,#把训练数据随机分批次
                                                shuffle=True, num_workers=0)
 
     # 10000张验证图片
@@ -24,20 +26,23 @@ def main():
                                            download=False, transform=transform)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=5000,
                                              shuffle=False, num_workers=0)
-    val_data_iter = iter(val_loader)
-    val_image, val_label = val_data_iter.next()
-    
-    # classes = ('plane', 'car', 'bird', 'cat',
-    #            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    val_data_iter = iter(val_loader)#把数据转化成可迭代的迭代器
+    val_image, val_label = val_data_iter.next()#通过next就可以一次获取一批数据及其label
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 
     net = LeNet()
+
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
-
+#训练过程
     for epoch in range(5):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        for step, data in enumerate(train_loader, start=0):
+        for step, data in enumerate(train_loader, start=0):#enumerate不仅可以返回每一批的data，还会返回对应的step
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
 
@@ -52,9 +57,9 @@ def main():
             # print statistics
             running_loss += loss.item()
             if step % 500 == 499:    # print every 500 mini-batches
-                with torch.no_grad():
+                with torch.no_grad():#with上下文管理器
                     outputs = net(val_image)  # [batch, 10]
-                    predict_y = torch.max(outputs, dim=1)[1]
+                    predict_y = torch.max(outputs, dim=1)[1]#在第一维去寻找最大值(预测标签)第0是batch.
                     accuracy = torch.eq(predict_y, val_label).sum().item() / val_label.size(0)
 
                     print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
@@ -63,9 +68,9 @@ def main():
 
     print('Finished Training')
 
-    save_path = './Lenet.pth'
-    torch.save(net.state_dict(), save_path)
-
-
+#     save_path = './Lenet.pth'
+#     torch.save(net.state_dict(), save_path)
+#
+#
 if __name__ == '__main__':
     main()
